9613597df23546f7ffa639ddb4f6b79384a8385c
jvsimino <joaovsmathias@gmail.com>
2021-09-25 12:42:39 -0300

    Correção Script

--- startArquiteturaMicroservices.sh
+++ startArquiteturaMicroservices.sh
@@ -4,10 +4,7 @@ apt -y update
 apt install -y openjdk-8-jdk openjdk-8-jre
 apt -y install ufw
 apt -y install postgresql
-apt -y install git 
- 
-git clone https://github.com/jvsimino/CloudOpss.git
- 
+
 service postgresql start 
 
 sh configure-firewall.sh
