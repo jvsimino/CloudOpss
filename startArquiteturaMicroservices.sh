a9e9d9781bd9ef31ced6f6baaf0fb35f2a0749ad
jvsimino <joaovsmathias@gmail.com>
2021-09-25 14:05:18 -0300

    Finalizando o script de instalação!

--- startArquiteturaMicroservices.sh
+++ startArquiteturaMicroservices.sh
@@ -8,22 +8,24 @@ apt -y install postgresql
 service postgresql start 
 
 sh configure-firewall.sh
-
-su - postgres
-createdb portal
-createdb cadastros
-createdb vendas
-createdb relatorios
-echo '\x \\ GRANT ALL ON portal TO postgres;' | psql
-echo '\x \\ GRANT ALL ON cadastros TO postgres;' | psql
-echo '\x \\ GRANT ALL ON vendas TO postgres;' | psql
-echo '\x \\ GRANT ALL ON relatorios TO postgres;' | psql
-
-exit
 sleep 5
+echo "createdb portal" | su - postgres
+echo "createdb cadastros" | su - postgres
+echo "createdb vendas" | su - postgres
+echo "createdb relatorios" | su - postgres
+
+#echo '\x \\ GRANT ALL ON portal TO postgres;' | su - postgres
+#echo '\x \\ GRANT ALL ON cadastros TO postgres;' | su - postgres
+#echo '\x \\ GRANT ALL ON vendas TO postgres;' | su - postgres
+#echo '\x \\ GRANT ALL ON relatorios TO postgres;' | su - postgres
+
+
+sleep 5
+
 echo "local all postgres peer" >> /etc/postgresql/10/main/pg_hba.conf
 
 service postgresql restart 
 
 sleep 10
 sh /CloudOpss/wildfly-8.2.1.Final/bin/start.sh
+
