b182b921eff77455d3e16120c883bdfdc15b138a
jvsimino <joaovsmathias@gmail.com>
2021-09-25 13:16:32 -0300

    Ajustes no script de start e Inclusão do README.md

--- startArquiteturaMicroservices.sh
+++ startArquiteturaMicroservices.sh
@@ -9,4 +9,21 @@ service postgresql start
 
 sh configure-firewall.sh
 
-#sh /CloudOpss/wildfly-8.2.1.Final/bin/start.sh
+su - postgres
+createdb portal
+createdb cadastros
+createdb vendas
+createdb relatorios
+echo '\x \\ GRANT ALL ON portal TO postgres;' | psql
+echo '\x \\ GRANT ALL ON cadastros TO postgres;' | psql
+echo '\x \\ GRANT ALL ON vendas TO postgres;' | psql
+echo '\x \\ GRANT ALL ON relatorios TO postgres;' | psql
+
+exit
+sleep 5
+echo "local all postgres peer" >> /etc/postgresql/10/main/pg_hba.conf
+
+service postgresql restart 
+
+sleep 10
+sh /CloudOpss/wildfly-8.2.1.Final/bin/start.sh
